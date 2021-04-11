ae0f59736fb58a78b3084ded63bb779de892dc76
Aditya Pawar <34043950+adityapawar1@users.noreply.github.com>
2021-04-10 18:41:21 -0700

    fixed typo lol
    it was really bothering me

--- src/Math/Math.js
+++ src/Math/Math.js
@@ -13,7 +13,7 @@ function Math(props){
             in elementary, middle and high school such as Math Kangaroo, MOEMS, Mathleague(Elementary, Middle, and High),
             AMC 8, AMC 10, and AIME to name a few. Although my interest in math was originally purely based on flexing
             my knowledge of the quadratic formula on friends ;), I've been transitioning to more research and proof based
-            mathematics by taking classes in analysis and proofs. I'veattended camps in Number Theory, Combinatorics 
+            mathematics by taking classes in analysis and proofs. I've attended camps in Number Theory, Combinatorics 
             and Linear Algebra while also taking online courses from MIT on Fourier Transforms and Differential Equations
             (Two pretty cool topics that are neatly related to Lin Alg). I am currently taking a course in Analysis 
             at Euler Circle which is a program that teaches high school students advanced mathematics. Although I don't plan
