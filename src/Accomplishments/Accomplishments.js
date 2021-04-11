3e3c64096331a8111e29d50fdd1d89923f3ba002
roshanbellary <40147115+roshanbellary@users.noreply.github.com>
2021-04-11 11:10:35 -0700

    Update Accomplishments.js

--- src/Accomplishments/Accomplishments.js
+++ src/Accomplishments/Accomplishments.js
@@ -10,7 +10,7 @@ class Accomplishments extends React.Component{
         return(
             <div>
                 <Header></Header>
-                <h1 class="Push"> HI</h1>
+                <h1 class="Push"> Accomplishments</h1>
                 <Footer></Footer>
             </div>
         );
